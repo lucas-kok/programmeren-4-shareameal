4f70e0b8309fa2608d941294158eaf4dd6860b11
Lucas Kok <56938053+lucas-kok@users.noreply.github.com>
2022-04-20 17:36:41 +0200

    first files

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+let database = [];
+let id = 0;
+
+app.all('*', (req, res, next) => {
+    const method = req.method;
+    console.log(`Method ${method} is aangeroepen`);
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        status: 200,
+        result: 'Hello World',
+    });
+});
+
+// Adding new movie
+app.post('/api/movie', (req, res) => {
+    let movie = req.body;
+    id++;
+    movie = {
+        id,
+        ...movie,
+    };
+    console.log(movie);
+    database.push(movie);
+    res.status(201).json({
+        status: 201,
+        result: database,
+    });
+});
+
+// Get movie with id
+app.get('/api/movie/:movieId', (req, res, next) => {
+    const movieId = req.params.movieId;
+    console.log(`Movie met ID ${movieId} gezocht`);
+    let movie = database.filter((item) => item.id == movieId);
+    if (movie.length > 0) {
+        console.log(movie);
+        res.status(200).json({
+            status: 200,
+            result: movie,
+        });
+    } else {
+        res.status(401).json({
+            status: 401,
+            result: `Movie with ID ${movieId} not found`,
+        });
+    }
+});
+
+// Get all movies
+app.get('/api/movie', (req, res, next) => {
+    res.status(200).json({
+        status: 200,
+        result: database,
+    });
+});
+
+// No valid request
+app.all('*', (req, res) => {
+    res.status(401).json({
+        status: 401,
+        result: 'End-point not found',
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
